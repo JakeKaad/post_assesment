3133bd35b27b9518e5886995c11bf58dc2b72f10
Jake Kaad <kaadalac@gmail.com>
2015-03-20 14:19:53 -0700

    Make default scope to be DESC

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,5 @@
 class Comment < ActiveRecord::Base
   belongs_to :post
   validates_presence_of :content
+  default_scope {order('created_at DESC')}
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,5 @@
 class Post < ActiveRecord::Base 
   has_many :comments
   validates_presence_of :title, :content
+  default_scope { order('updated_at DESC') }
 end
