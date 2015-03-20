c0fa382eaeebc7c9702404e6816456372b9e776e
Jake Kaad <kaadalac@gmail.com>
2015-03-20 09:53:02 -0700

    Add post controller actions index and show with initialization specs

--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -1,1 +1,28 @@
 require 'rails_helper'
+
+describe PostsController do 
+
+  describe 'GET index' do 
+    it "sets @posts" do 
+      post = Fabricate(:post)
+      get :index
+      expect(assigns(:posts)).to eq([post])
+    end
+  end
+
+  describe 'GET show' do 
+    it "sets @post to the correct post" do 
+      post = Fabricate(:post)
+      get :show, id: post.id 
+      expect(assigns(:post)).to eq(post)
+    end
+
+    it "doesn't set @post to the incorrect post" do 
+      post = Fabricate(:post)
+      not_post = Fabricate(:post)
+      get :show, id: post.id 
+      expect(assigns(:post)).to_not eq(not_post)
+    end
+  end
+
+end
