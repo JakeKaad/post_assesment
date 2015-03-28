f55fb360aca418f2a2c92fa5733abe3b60005e55
Jake Kaad <kaadalac@gmail.com>
2015-03-28 09:30:00 -0700

    Add relation and specs for user with comments and posts

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users do
+    resources :posts do
+      resources :comments, only: [:create, :destroy]
+    end
+  end
   root to: 'posts#index'
 
-  resources :posts do 
+  resources :posts do
     resources :comments, only: [:create, :destroy]
   end
 end
